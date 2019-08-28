65373b6cc66dfeb5829f9bb988319aabed8aaf31
skynetshrugged <46287984+skynetshrugged@users.noreply.github.com>
2019-08-28 21:42:53 +0100

    This is all messed up - dont try and run this now.,
    Messed this up whilst practice cs50

--- run_examples.py
+++ run_examples.py
@@ -1,55 +1,82 @@
-import argparse
-import logging
-import os
-import sys
+
+s = "jajkfhdjbaoioiebcjhb"
+count = 0
+
+for x in s:
+    if x in "aeiou":
+        count+=1
+print(count)
+
+d = "aeiou"
+
+c = 0
+for x in s:
+    if x in d:
+        c+=1
+print(c)
+
+print(list(s))
+
+y = dict()
+t = list(s)
+z = []
+for x in t:
+    if x in "aeiou":
+        y[x] = y.get(x, 0) + 1
+for x,y in y.items():
+    z.append( (y,x) )
+
+#z.sort()
+
+for x in z:
+    print(x)
 
 
-parser = argparse.ArgumentParser(description='Run example AI programs.')
-parser.add_argument('--log', action='store_true', help='Turn on logging')
+
+s = 'azcbobobbegbobghakl'
+
+counter = 0
+index = 0
+while index < len(s):
+    index = s.find("bob", index)
+    if index == -1:
+        break
+    print("bob found at", index)
+    counter +=1
+    index += 2
+print("Number of times bob occurs is:", counter)
+
+# import regex as re
+#
+# x = re.findall("bob", s, overlapped=True)
+# print("Number of times bob occurs is:",len(x))
 
 
-def main():
-    print ('Please choose an example to run:')
-    modules = []
-    for i, name in enumerate(discover_modules('paip/examples')):
-        __import__(name)
-        module = sys.modules[name]
-        modules.append(module)
-        print ('%d\t%s' % (i, module.__name__))
-    while True:
-        try:
-            choice = raw_input('>> ')
-            if not choice:
-                continue
-            ind = int(choice)
-        except ValueError:
-            print ('That is not a valid option.  Please try again.')
-            continue
-        except EOFError:
-            print ('Goodbye.')
-            return
-        try:
-            module = modules[ind]
-        except IndexError:
-            print ('That is not a valid option.  Please try again.')
-        else:
-            print (module.__doc__)
-            return module.main()
 
 
-def discover_modules(root):
-    modules = []
-    for root, dirs, files in os.walk(root):
-        for f in (f for f in files if f.endswith('.py') and '__init__' not in f):
-            path = os.path.join(root, f)[:-3]
-            if root.startswith('.'):
-                path = path[2:]
-            modules.append(path.replace(os.sep, '.'))
-    return modules
+
+# Assume s is a string of lower case characters.
+#
+# Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
+#
+# Longest substring in alphabetical order is: beggh
+# In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
+#
+# Longest substring in alphabetical order is: abc
+
+for i in range(5):
+    print(i)
 
 
-if __name__ == '__main__':
-    args = vars(parser.parse_args())
-    if args['log']:
-        logging.basicConfig(level=logging.DEBUG)
-    main()
+a = 2*2
+print(f"Hello World {a}\nI Love you!")
+
+x = 2
+y = 5
+
+# x = int(input("Please enter x value: "))
+# y = int(input("Please enter y value: "))
+
+print(f"{x} * {y} = {x * y}!\n Well done!")
+
+print(2/10 == 0.3)
